99fc065da80b959d4145dc0878504ae670aaa0d9
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-07-28 07:52:28 +0300

    feat: create base user models

--- library/users/models.py
+++ library/users/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Librarian(AbstractUser):
+    table_number = models.CharField(
+        "Табельный номер",
+        max_length=10,
+    )
+
+    class Meta:
+        verbose_name = "Библиотекарь"
+        verbose_name_plural = "Библиотекари"
+
+    def __str__(self):
+        return self.username
+
+
+class Reader(AbstractUser):
+    address = models.CharField(
+        "Адрес проживания",
+        max_length=100,
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Читатель"
+        verbose_name_plural = "Читатели"
+
+    def __str__(self):
+        return self.username

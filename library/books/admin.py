1937e252c421e41389046c10ed7aae82f7c6b966
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-07-29 10:42:43 +0300

    feat: add basic admin config

--- library/books/admin.py
+++ library/books/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from books.models import Book
+
+
 # Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "author",
+        "genre",
+        "year",
+        "cover",
+        "is_borrowed",
+        "borrowed_at",
+        "reader",
+    )
+    list_filter = ("is_borrowed",)
--- library/users/admin.py
+++ library/users/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import Librarian, Reader
+
+
+#
+#
+@admin.register(Librarian)
+class LibrarianAdmin(UserAdmin):
+    readonly_fields = ("table_number",)
+    list_display = ("username", "first_name", "last_name", "table_number")
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Персональная информация",
+         {"fields": ("first_name", "last_name", "email", "table_number")}),
+        (
+            "Права доступа",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Важные даты", {"fields": ("last_login", "date_joined")}),
+    )
+
+    @admin.action(description="Табличный номер")
+    def table_number(self, obj):
+        return obj.lib_extra_fields.table_number
+
+
+@admin.register(Reader)
+class ReaderAdmin(UserAdmin):
+    readonly_fields = ("address",)
+    list_display = ("username", "first_name", "last_name", "address",)
+
+    @admin.action(description="Адрес проживания")
+    def address(self, obj):
+        return obj.reader_extra_fields.address
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Персональная информация",
+         {"fields": ("first_name", "last_name", "email", "address")}),
+        (
+            "Права доступа",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Важные даты", {"fields": ("last_login", "date_joined")}),
+    )

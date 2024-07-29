17c73b8568cdb81a0fe6d34f4a7fdf62cae0a235
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-07-29 10:46:05 +0300

    feat: update models for project entities. First iteration

--- library/books/models.py
+++ library/books/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField("Название", max_length=100)
+    author = models.CharField("Автор", max_length=100)
+    genre = models.CharField("Жанр", max_length=100)
+    year = models.IntegerField("Год издания")
+    cover = models.ImageField("Обложка", upload_to="covers/")
+    is_borrowed = models.BooleanField(
+        "Книга взята в прочтение",
+        default=False
+    )
+    borrowed_at = models.DateTimeField(
+        "Дата взятия книги",
+        null=True,
+        blank=True
+    )
+    reader = models.ForeignKey(
+        "users.User",
+        on_delete=models.SET_NULL,
+        related_name="books",
+        verbose_name="Читатель",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Книга"
+        verbose_name_plural = "Книги"
+
+    def __str__(self):
+        return self.title
--- library/users/models.py
+++ library/users/models.py
@@ -2,21 +2,71 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class Librarian(AbstractUser):
-    table_number = models.CharField(
-        "Табельный номер",
+class LibrarianManager(models.Manager):
+
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(
+            role=User.Roles.LIBRARIAN
+        )
+
+
+class ReaderManager(models.Manager):
+
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(
+            role=User.Roles.READER
+        )
+
+
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        LIBRARIAN = 'LIBRARIAN', 'Библиотекарь'
+        READER = 'READER', 'Читатель'
+
+    base_type = Roles.READER
+
+    role = models.CharField(
         max_length=10,
+        choices=Roles.choices,
+        default=base_type
     )
+
+    @property
+    def is_librarian(self):
+        return self.role == "LIBRARIAN"
+
+    @property
+    def is_reader(self):
+        return self.role == "READER"
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.type = self.base_type
+        return super().save(*args, **kwargs)
+
+
+class LibExtraFields(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    table_number = models.IntegerField(default=0)
+
+
+class Librarian(User):
+    objects = LibrarianManager()
+
+    base_type = User.Roles.LIBRARIAN
 
     class Meta:
         verbose_name = "Библиотекарь"
         verbose_name_plural = "Библиотекари"
+        proxy = True
 
-    def __str__(self):
-        return self.username
+    @property
+    def lib_extra_fields(self):
+        return self.libextrafields
 
 
-class Reader(AbstractUser):
+class ReaderExtraFields(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     address = models.CharField(
         "Адрес проживания",
         max_length=100,
@@ -24,9 +74,17 @@ class Reader(AbstractUser):
         null=True,
     )
 
+
+class Reader(User):
+    objects = ReaderManager()
+
+    base_type = User.Roles.READER
+
     class Meta:
         verbose_name = "Читатель"
         verbose_name_plural = "Читатели"
+        proxy = True
 
-    def __str__(self):
-        return self.username
+    @property
+    def reader_extra_fields(self):
+        return self.readerextrafields

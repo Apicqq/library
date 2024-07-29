ebab1a0c76354f7d8732eb4c7b40b1fa4486e4ab
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-07-29 21:40:38 +0300

    feat: move javascript to a separate file

--- library/templates/includes/scripts/copy-text.js
+++ library/templates/includes/scripts/copy-text.js
@@ -1,4 +1,4 @@
-const _buttons = document.getElementsByClassName('btn btn-outline-primary 123');
+const _buttons = document.getElementsByClassName('btn btn-outline-secondary');
 for (let i = 0; i < _buttons.length; i++) {
     _buttons[i].addEventListener('click', function () {
         const cardTitle = this.parentNode.parentNode.querySelector('.card-title').innerHTML;

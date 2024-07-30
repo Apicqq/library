432adc84af4be450df1084eb5dc3deba377bf3aa
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-07-30 15:58:22 +0300

    refactor: refactor signal logic

--- library/users/signals.py
+++ library/users/signals.py
@@ -7,15 +7,15 @@ from users.models import User, ReaderExtraFields, LibExtraFields
 @receiver(post_save, sender=User, dispatch_uid="create_user_address")
 def populate_extra_fields(sender, instance, created, **kwargs):
     if created:
-        if instance.role == "READER":
-            created_data = ReaderExtraFields.objects.create(
+        if instance.is_reader:
+            obj, created = ReaderExtraFields.objects.update_or_create(
                 user=instance,
                 address=instance.address
             )
-            created_data.save()
+            # data.save()
         else:
-            created_data = LibExtraFields.objects.create(
+            obj, created = LibExtraFields.objects.update_or_create(
                 user=instance,
                 table_number=instance.table_number
             )
-            created_data.save()
+            # data.save()

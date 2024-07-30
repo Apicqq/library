3ced44b16856f84dabb5f744075699386b45c81b
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-07-30 15:57:22 +0300

    feat: add constants to project

---
+++ library/core/constants.py
@@ -0,0 +1,40 @@
+from enum import Enum
+
+
+class BookConstants(str, Enum):
+    TITLE = "Название"
+    AUTHOR = "Автор"
+    GENRE = "Жанр"
+    YEAR = "Год издания"
+    COVER = "Обложка"
+    IS_BORROWED = "Книга взята в прочтение"
+    BORROWED_AT = "Дата взятия в прочтение"
+    READER = "Читатель"
+    BOOK = "Книга"
+    BOOKS = "Книги"
+    ADDED_AT = "Книга добавлена в картотеку"
+
+
+class Errors(str, Enum):
+    NOT_AUTHORIZED_TO_RENT = "У вас нет прав для добавления книг в картотеку."
+    BOOK_IS_RENTED = ("Вы не можете взять эту книгу в аренду, т.к. она уже "
+                      "занята другим читателем.")
+    INVALID_BOOK_YEAR = "Год выпуска книги не может быть больше текущего."
+
+
+class UserConstants(str, Enum):
+    LIBRARIAN = "Библиотекарь"
+    LIBRARIANS = "Библиотекари"
+    READER = "Читатель"
+    READERS = "Читатели"
+    FIRST_NAME = "Имя"
+    LAST_NAME = "Фамилия"
+    TABLE_NUMBER = "Табельный номер"
+    ADDRESS = "Адрес проживания"
+    HAS_BOOKS_RENTED = "Есть книги на руках"
+    ROLE = "Роль"
+    NON_REQUIRED_FIELD = "Необязательное поле"
+    CHOOSE_YOUR_ROLE = "Выберите роль пользователя."
+    FIELD_FOR_READERS = "Для читателей."
+    FIELD_FOR_LIBRARIANS = "Для библиотекарей."
+    EVER_RENTED_A_BOOK = "Когда-либо брал книгу напрокат"
